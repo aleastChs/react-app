a5b1fe0c82618825ee6797061ef5f36b830aa7a7
aleastChs <aleast@student.chalmers.se>
2018-07-10 17:23:18 +0200

    Local changes

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var config = {
+    entry: './main.js',
+    output: {
+        path: '/',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'bable-loader',
+            query: {
+                prestes: ['es2015', 'react']
+            }
+        }]
+    }
+}
+module.exports = config;
