b653dfba5a7ffbfbeda91b5ac1c3a1abf57df844
aleastChs <aleast@student.chalmers.se>
2018-07-12 18:16:14 +0200

    Minor jsx import change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header.jsx';
 import TableRow from './TableRow.jsx';
 
-class App extends React.Component {
+class App extends Component {
     constructor() {
       super();
       this.state = {
